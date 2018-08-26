4f3e509a278fd5987f5c22d33f8d3f5d16a19244
Archy Mathur <archy2212@gmail.com>
2018-08-26 23:27:09 +0530

    update

--- MyClass.java
+++ MyClass.java
@@ -4,7 +4,8 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is my first java!");
+		int a=10;
+		System.out.println("this is my first java!"+a);
 	}
 
 }
