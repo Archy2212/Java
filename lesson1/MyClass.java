3512bc26c2f32a954279d8f25c7b0e989e1dea9e
Archy Mathur <archy2212@gmail.com>
2018-08-27 22:48:15 +0530

    day27 code1

---
+++ lesson1/MyClass.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first java !");
+	}
+
+}
