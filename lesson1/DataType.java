686f7624639d0525ead4e7b82fea38ae9ecc18b2
Archy Mathur <archy2212@gmail.com>
2018-08-27 22:49:00 +0530

    day27 code2

---
+++ lesson1/DataType.java
@@ -0,0 +1,21 @@
+package lesson1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;//4byte byte(1byte)
+		short b=9;//2byte long(8byte)
+		float c=(float)2.9;//4byte(initially taken as double)
+		double d=11.89;//8byte
+		char e='A';//2byte
+		boolean f=true;//1byte
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+	}
+
+}
