bffcdf589d257ea796b9276100b2039a8e94bd92
Archy Mathur <archy2212@gmail.com>
2018-09-04 22:43:34 +0530

    day 35 code 2

---
+++ lesson1/MathOperators.java
@@ -0,0 +1,22 @@
+package lesson1;
+
+public class MathOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,y,sum,diff,div,mul,mod;
+		x=30;
+		y=20;
+		sum=x+y;
+		System.out.println(sum);
+		mul=x*y;
+		System.out.println(mul);
+		div=x/y;
+		System.out.println(div);
+		diff=x-y;
+		System.out.println(diff);
+		mod=x%y;
+		System.out.println(mod);
+	}
+
+}
