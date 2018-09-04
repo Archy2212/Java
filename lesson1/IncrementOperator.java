675be99a75fbcb00f93f3d80ec871a7d2e00e48c
Archy Mathur <archy2212@gmail.com>
2018-09-04 22:49:46 +0530

    day 35 code 3

---
+++ lesson1/IncrementOperator.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public class IncrementOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,sum;
+		x=30;
+		//System.out.println(x++);   //output=30 post increment
+		System.out.println(++x);	//output=31	 pre increment
+		System.out.println(x);		//output=31
+		
+		
+	}
+
+}
