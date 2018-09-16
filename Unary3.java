69ccc88053f62f8d43cc4f92167e196d5a7ccb1c
Archy Mathur <archy2212@gmail.com>
2018-09-16 23:20:54 +0530

    java

---
+++ Unary3.java
@@ -0,0 +1,14 @@
+public class Unary3
+{
+    public static void main(String args[])
+    {
+        int a=12;
+        int b=-10;
+        boolean c=true;
+        boolean d=false;
+        System.out.println(~a);
+        System.out.println(~b);
+        System.out.println(!c);
+        System.out.println(!d);
+    }
+}
