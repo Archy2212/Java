229de13058792c7f202b740e5e0c6fb3dc12353f
Archy Mathur <archy2212@gmail.com>
2018-09-16 23:22:08 +0530

    java

---
+++ Unary4.java
@@ -0,0 +1,10 @@
+public class Unary4
+{
+    public static void main(String args[])
+    {
+     System.out.println(10<<2);
+     System.out.println(10<<3);
+     System.out.println(20<<2);
+     System.out.println(15<<4);
+    }
+}
