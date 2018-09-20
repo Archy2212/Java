4c4c13765e04c4c270605c90f931ed37227a44d5
Archy Mathur <archy2212@gmail.com>
2018-09-20 23:28:40 +0530

    day53

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+public class EvenOdd
+{
+    public static void main(String args[])
+    {
+        int n=3;
+        if(n%2==0)
+        
+            System.out.println("Even");
+        
+        else
+        
+            System.out.println("Odd");
+        
+    }
+}
