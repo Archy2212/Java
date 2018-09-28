d8d7e5f1fb5eddc7e114c4cf4f19677e073d3817
Archy2212 <40794153+Archy2212@users.noreply.github.com>
2018-09-28 22:51:08 +0530

    Create BreakEx.java

---
+++ BreakEx.java
@@ -0,0 +1,16 @@
+public class BreakEx
+{
+    public static void main(String args[])
+    {
+        int i;
+        for(i=1;i<10;i++)
+        {
+            if(i==5)
+            {
+            break;
+            }
+        
+        System.out.println(i);
+        }
+    }
+}
