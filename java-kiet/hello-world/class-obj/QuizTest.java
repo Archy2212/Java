14a1df908ca2c26c062bc14da79749c43d43060b
Archy Mathur <archy2212@gmail.com>
2018-10-14 23:03:46 +0530

    day77

---
+++ java-kiet/hello-world/class-obj/QuizTest.java
@@ -0,0 +1,8 @@
+public class QuizTest{
+    public static void main(String[] args) {
+        Question q1 = new Question();
+        q1.var1 = 2;
+        q1.var2 =3;
+        System.out.println(q1.sum(2, 5));
+    }
+}
