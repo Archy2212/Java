7ed17a1e94ca5da04712f746fbe08a4add5fe5cf
Archy Mathur <archy2212@gmail.com>
2018-10-14 23:04:03 +0530

    day77

---
+++ java-kiet/hello-world/strings/StringVar.java
@@ -0,0 +1,14 @@
+public class StringVar{
+    public static void main(String[] args) {
+        String firstName = "Chetan";
+        String lastName = "Vashistth";
+        System.out.println(firstName+ " " + lastName);
+
+        // how  toLowerCase() and toUpperCase() works?
+
+        System.out.println(firstName.toUpperCase());
+        System.out.println(lastName.toLowerCase());
+
+    }
+    
+}
