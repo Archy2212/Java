061dfaf21168c4ff16afe766d5d48fddbbd76459
Archy2212 <40794153+Archy2212@users.noreply.github.com>
2018-10-31 23:28:38 +0530

    Update BasicCal.java

--- calculator/BasicCal.java
+++ calculator/BasicCal.java
@@ -2,7 +2,7 @@ public class BasicCal{
     public static void main(String[] args) {
         System.out.println(5+3);
         System.out.println(5*3);
-        System.out.println(3-2);   
+        System.out.println(4-2);   
     }   
     
 }
