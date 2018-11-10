94cc69e92422ac1286a66a30cfa06aa06b45e6bc
Archy2212 <40794153+Archy2212@users.noreply.github.com>
2018-11-10 23:46:25 +0530

    Update StringVar.java

--- strings/StringVar.java
+++ strings/StringVar.java
@@ -4,7 +4,6 @@ public class StringVar{
         String lastName = "Mathur";
         System.out.println(firstName+ " " + lastName);
         // how toLowerCase() and toUpperCase() works??
-
         System.out.println(firstName.toUpperCase());
         System.out.println(lastName.toLowerCase());
 
