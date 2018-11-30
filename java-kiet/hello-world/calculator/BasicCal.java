40d450085ec16c71c9e9f56462d6b0e83b2ed28d
Archy2212 <40794153+Archy2212@users.noreply.github.com>
2018-11-30 23:43:16 +0530

    Update BasicCal.java

--- java-kiet/hello-world/calculator/BasicCal.java
+++ java-kiet/hello-world/calculator/BasicCal.java
@@ -2,8 +2,7 @@ public class BasicCal
 {
     public static void main(String[] args)
     {
-        
-        System.out.println(3+3);
+               System.out.println(3+3);
         System.out.println(5*3);
         System.out.println(5-3);
     }
